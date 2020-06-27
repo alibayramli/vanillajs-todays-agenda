1486871f8632647235eea55bbad3b29bcaf11b07
Ali Bayramli <alibayramli97@gmail.com>
2020-06-27 14:00:04 +0200

    Enable localStorage

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const input = document.querySelector(".input");
 const button = document.querySelector(".button");
 const list = document.querySelector(".list");
 const filter = document.querySelector('.filter-list');
-
+document.addEventListener("DOMContentLoaded", getListsFromLocalStorage);
 button.addEventListener("click", addLists);
 list.addEventListener('click', deleteLists);
 filter.addEventListener('click', filterLists);
@@ -16,6 +16,7 @@ function addLists(event) {
         newLi.textContent = input.value; // could also be innerText
         newLi.classList.add('li-item');
         addedDiv.appendChild(newLi);
+        saveListsToLocal(input.value);
         // for check box
         const doneBtn = document.createElement('button');
         doneBtn.innerHTML = `<i class="fas fa-check"></i>`;
@@ -29,16 +30,17 @@ function addLists(event) {
         // deleteBtn.addEventListener('click', (e) => {
         //     addedDiv.remove()
         // })
-        list.append(addedDiv);
+        list.appendChild(addedDiv);
         input.value = "";
     }
-
 }
 function deleteLists(event) {
     const item = event.target;
     if (item.classList[0] === 'delete-btn') {
         const parentItem = item.parentElement;
         parentItem.classList.add('fall');
+        console.log(parentItem.children[0].textContent)
+        removeLocalList(parentItem);
         parentItem.addEventListener('transitionend', () => {
             parentItem.remove();
         })
@@ -88,4 +90,54 @@ function filterLists(e) {
         }
     })
 }
+function saveListsToLocal(list) {
+    let lists;
+    if (localStorage.getItem('todos') === null) {
+        lists = [];
+    } else {
+        lists = JSON.parse(localStorage.getItem('todos'));
+    }
+    lists.push(list.trim());
+    localStorage.setItem('todos', JSON.stringify(lists));
+}
 
+function getListsFromLocalStorage() {
+    let lists;
+    if (localStorage.getItem('todos') === null) {
+        lists = [];
+    } else {
+        lists = JSON.parse(localStorage.getItem('todos'));
+    }
+    lists.forEach((localList) => {
+        const addedDiv = document.createElement('div');
+        addedDiv.classList.add("todo");
+        const newLi = document.createElement('li');
+        newLi.textContent = localList; // could also be innerText
+        newLi.classList.add('li-item');
+        addedDiv.appendChild(newLi);
+        // for check box
+        const doneBtn = document.createElement('button');
+        doneBtn.innerHTML = `<i class="fas fa-check"></i>`;
+        doneBtn.classList.add("done-btn");
+        addedDiv.appendChild(doneBtn);
+        // for deletion
+        const deleteBtn = document.createElement('button');
+        deleteBtn.innerHTML = `<i class="fas fa-trash"></i>`;
+        deleteBtn.classList.add("delete-btn");
+        addedDiv.appendChild(deleteBtn);
+        list.appendChild(addedDiv);
+    })
+}
+
+function removeLocalList(localList) {
+    let lists;
+    if (localStorage.getItem('todos') === null) {
+        lists = [];
+    } else {
+        lists = JSON.parse(localStorage.getItem('todos'));
+    }
+    const index = localList.children[0].innerText;
+    console.log(lists.indexOf(index))
+    lists.splice(lists.indexOf(index), 1);
+    localStorage.setItem('todos', JSON.stringify(lists));
+}
